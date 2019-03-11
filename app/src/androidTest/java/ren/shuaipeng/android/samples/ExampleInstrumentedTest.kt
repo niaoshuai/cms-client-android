c297c5a6c29446d42867b07c8d31cb1d7bcf8dbc
niaoshuai <niao.shuai123@163.com>
2019-03-11 17:35:46 +0800

    Update Test

--- app/src/androidTest/java/ren/shuaipeng/android/samples/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ren/shuaipeng/android/samples/ExampleInstrumentedTest.kt
@@ -1,22 +1,23 @@
 package ren.shuaipeng.android.samples
 
-import androidx.test.InstrumentationRegistry
-import androidx.test.runner.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
+@RunWith(Parameterized::class)
 class ExampleInstrumentedTest {
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
+        val appContext = getInstrumentation().targetContext
         assertEquals("ren.shuaipeng.android.samples", appContext.packageName)
     }
 }
